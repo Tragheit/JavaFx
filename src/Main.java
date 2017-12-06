d3fc6db80b3b4335e699a895514fade60e8dfbe3
Tragheit <tragheit@tutorials.com>
2017-12-07 00:30:04 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    //first button
+    Button button;
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //setting the name of Stage
+        //Stage is just blank window
+        primaryStage.setTitle("Title of the Window");
+
+        //creating layout
+        //Layout is the way the elements are arranged
+        StackPane layout = new StackPane();
+
+        //creating scene
+        //Scene is the content of the window (buttons, text fields etc.)
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+
+        //display of Stage to the user
+        primaryStage.show();
+
+        //using our first button
+        button = new Button();
+
+        //setting text on button
+        button.setText("Click me");
+
+        //adding our first button to the main screen
+        layout.getChildren().add(button);
+
+
+    }
+}
+
