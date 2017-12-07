d7dae076569118366339973d9c09c4a02b7ebdd2
Tragheit <tragheit@tutorials.com>
2017-12-07 20:07:39 +0100

    lesson fourteen - listview

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -11,7 +13,7 @@ public class Main extends Application {
     private Stage window;
     private Scene scene;
     private Button button;
-    private ComboBox<String> comboBox;
+    private ListView<String> listView;
 
     public static void main(String[] args) {
         launch(args);
@@ -23,36 +25,38 @@ public class Main extends Application {
         window.setTitle("ComboBox Demo");
         button = new Button("Submit");
 
-        comboBox = new ComboBox<>();
-        comboBox.getItems().addAll(
-                "Good Will Hunting",
-                "St. Wincent",
-                "Blackhat"
-        );
 
-        comboBox.setPromptText("What is your favourite movie? ");
+        listView = new ListView<>();
+        listView.getItems().addAll("Iron Man", "Titanic", "Contact", "Surrogates");
 
-        //User is able to enter title of his favourite movie from keyboard
-        comboBox.setEditable(true);
+        //Decide whether one or multiple selection are allowed
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-        //comboBoxes generates it's own events. No need to implement listeners
-        comboBox.setOnAction(e -> System.out.println("User selected: " + comboBox.getValue()));
+        button.setOnAction(e -> buttonClicked());
 
-        button.setOnAction(e -> printMovie());
 
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(comboBox, button);
+        layout.getChildren().addAll(listView, button);
 
-        scene = new Scene(layout);
+        scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
 
-    //Print out a movie
-    private void printMovie() {
-        System.out.println(comboBox.getValue());
+    //Print out movies
+    private void buttonClicked(){
+        String message = "";
+        //all Lists in JavaFX are ObservableLists <- main class
+        ObservableList<String> movies;
+        movies = listView.getSelectionModel().getSelectedItems();
+
+        for (String m: movies) {
+            message += m + "\n";
+        }
+
+        System.out.println(message);
     }
 }
 
