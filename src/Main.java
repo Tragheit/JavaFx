9170f20b30eaaaa30ecddae76d02fe2070a4dd1a
Tragheit <tragheit@tutorials.com>
2017-12-07 17:21:13 +0100

    lesson eight - login screen using gridpane

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,27 +20,46 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("Main window");
 
-        //Horizontal box, (should not be used to create horizontal menus)
-        HBox topMenu = new HBox();
-        Button buttonA = new Button("FIle");
-        Button buttonB = new Button("Edit");
-        Button buttonC = new Button("View");
-        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
+        //special layout for arrange items in a grid
+        GridPane grid = new GridPane();
 
-        //Vertical box
-        VBox leftMenu = new VBox();
-        Button buttonD = new Button("D");
-        Button buttonE = new Button("E");
-        Button buttonF = new Button("F");
-        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+        /* Insets - object which stores the amount of padding for each edge
+           adds 10 pixels padding around window */
+        grid.setPadding(new Insets(50, 50, 50, 50));
 
-        //special layout divided on (top, down, left, right, center) sections
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(topMenu);
-        borderPane.setLeft(leftMenu);
+        //sets the vertical spacing or the padding of vertically lines
+        grid.setVgap(8);
+        //sets the horizontal spacing or the padding of vertically lines
+        grid.setHgap(10);
 
-        Scene scene = new Scene(borderPane, 300, 250);
+        //Name label
+        Label nameLabel = new Label("Username");
+
+        //adding items to the grid cells
+        GridPane.setConstraints(nameLabel, 0, 0);
+
+        //Name input
+        TextField nameInput = new TextField("default login");
+        GridPane.setConstraints(nameInput, 1, 0);
+
+        //Password Label
+        Label passLabel = new Label("Password");
+        GridPane.setConstraints(passLabel, 0, 1);
+
+        //Password Input
+        TextField passInput = new TextField();
+        //light grayed out text inside textfield
+        passInput.setPromptText("password");
+        GridPane.setConstraints(passInput, 1, 1);
+
+        Button loginButton = new Button("Login");
+        GridPane.setConstraints(loginButton, 1, 2);
+
+        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
+
+        Scene scene = new Scene(grid, 350, 200);
         window.setScene(scene);
+
         window.show();
 
     }
