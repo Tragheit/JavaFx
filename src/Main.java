576d4fee1ce01209bc17eac4b04cd4d8490e75e7
Tragheit <tragheit@tutorials.com>
2017-12-07 16:49:03 +0100

    lesson seven - embedding layouts

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,47 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    Stage window;
-    Button button;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
+        Stage window;
+
         window = primaryStage;
         window.setTitle("Main window");
 
-        //executes safe exit when "x" clicked
-        window.setOnCloseRequest(e -> {
-            e.consume();
-            closeProgram();
-        });
+        //Horizontal box, (should not be used to create horizontal menus)
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("FIle");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
 
-        button = new Button("Close Program");
+        //Vertical box
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
 
-        //executes safe exit when button clicked
-        button.setOnAction(e -> closeProgram());
+        //special layout divided on (top, down, left, right, center) sections
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
 
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene1 = new Scene(layout, 300, 250);
-        window.setScene(scene1);
+        Scene scene = new Scene(borderPane, 300, 250);
+        window.setScene(scene);
         window.show();
-    }
 
-    private void closeProgram() {
-        Boolean answer = ConfirmBox.display("Title", "Sure you want exit");
-
-        if(answer) {
-            window.close();
-        }
     }
 }
 
