9079cc634b34d655dd2767d8769f622f5d4bf3b3
Tragheit <tragheit@tutorials.com>
2017-12-07 16:16:06 +0100

    lesson five - confirmation window

--- src/Main.java
+++ src/Main.java
@@ -11,23 +11,26 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Stage window;
-    Button button1;
+    Button button;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage){
         window = primaryStage;
         window.setTitle("Main window");
+        button = new Button("Display Confirmation Window");
 
-        button1 = new Button("Show alert box");
-        button1.setOnAction(e -> AlertBox.display("Alert Box", "Wow this alert box is awesome"));
+        button.setOnAction(e -> {
+            boolean result = ConfirmBox.display("Conformation Window","Are you sure to save changes? ");
+            System.out.println(result);
+        });
 
-        StackPane layout1 = new StackPane();
-        layout1.getChildren().add(button1);
-        Scene scene1 = new Scene(layout1, 300, 250);
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene1 = new Scene(layout, 300, 250);
         window.setScene(scene1);
         window.show();
 
