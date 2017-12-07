56f263bff7eb63cd51a8ccde4473388a22ccfcaa
Tragheit <tragheit@tutorials.com>
2017-12-07 17:37:08 +0100

    lesson nine - extract and validate input

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,51 +15,38 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Stage window;
+        Scene scene;
+        Button button;
 
         window = primaryStage;
         window.setTitle("Main window");
 
-        //special layout for arrange items in a grid
-        GridPane grid = new GridPane();
+        //Form
+        TextField nameInput = new TextField();
 
-        /* Insets - object which stores the amount of padding for each edge
-           adds 10 pixels padding around window */
-        grid.setPadding(new Insets(50, 50, 50, 50));
+        button = new Button("Click me");
+        button.setOnAction(e -> isInt(nameInput));
 
-        //sets the vertical spacing or the padding of vertically lines
-        grid.setVgap(8);
-        //sets the horizontal spacing or the padding of vertically lines
-        grid.setHgap(10);
+        //Layout
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.getChildren().addAll(nameInput, button);
 
-        //Name label
-        Label nameLabel = new Label("Username");
-
-        //adding items to the grid cells
-        GridPane.setConstraints(nameLabel, 0, 0);
-
-        //Name input
-        TextField nameInput = new TextField("default login");
-        GridPane.setConstraints(nameInput, 1, 0);
-
-        //Password Label
-        Label passLabel = new Label("Password");
-        GridPane.setConstraints(passLabel, 0, 1);
-
-        //Password Input
-        TextField passInput = new TextField();
-        //light grayed out text inside textfield
-        passInput.setPromptText("password");
-        GridPane.setConstraints(passInput, 1, 1);
-
-        Button loginButton = new Button("Login");
-        GridPane.setConstraints(loginButton, 1, 2);
-
-        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
-
-        Scene scene = new Scene(grid, 350, 200);
+        scene = new Scene(layout, 300, 250);
         window.setScene(scene);
-
         window.show();
+    }
+
+    private boolean isInt (TextField input){
+        try{
+            int age = Integer.parseInt(input.getText());
+            System.out.println("User is: " + age);
+            return true;
+        }catch (NumberFormatException e) {
+            System.out.println("Error: " + input.getText() + " is not a number");
+            return false;
+        }
+
 
     }
 }
