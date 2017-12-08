64d4f7f933afefa0e62ae2d81a8f5b2f2696a9b0
Tragheit <tragheit@tutorials.com>
2017-12-08 23:46:58 +0100

    lesson eighteen - handling menu clicks

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,17 @@
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     private Stage window;
     private Scene scene;
-    private TableView<Product> table;
-    private TextField nameInput, priceInput, quantityInput;
+    private BorderPane layout;
 
     public static void main(String[] args) {
         launch(args);
@@ -26,97 +20,68 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
-        window.setTitle("TableView Demo");
-
-        //Name column
-        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setMinWidth(200);
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
-        //Price column
-        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
-        priceColumn.setMinWidth(100);
-        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-
-        //Quantity column
-        TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
-        quantityColumn.setMinWidth(200);
-        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
-
-        //Name input
-        nameInput = new TextField();
-        nameInput.setPromptText("Name");
-        nameInput.setMinWidth(100);
-
-        //Price input
-        priceInput = new TextField();
-        priceInput.setPromptText("Price");
-        priceInput.setMinWidth(100);
-
-        //Quantity input
-        quantityInput = new TextField();
-        quantityInput.setPromptText("Quantity");
-        quantityInput.setMinWidth(100);
-
-        //Button
-        Button addButton = new Button("Add");
-        addButton.setOnAction(e -> addButtonClicked());
-        Button deleteButton = new Button("Delete");
-        deleteButton.setOnAction(e -> deleteButtonClicked());
-
-        //Layout
-        HBox hBox = new HBox();
-        hBox.setPadding(new Insets(10, 10, 10, 10));
-        hBox.setSpacing(10);
-        hBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, deleteButton);
-
-        table = new TableView<>();
-        table.setItems(getProduct());
-        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
+        window.setTitle("Menu Demo");
 
 
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(table, hBox);
 
-        scene = new Scene(vBox);
+        //          file menu
+        //underline before menuitem name means that user can use shortcut alt _ first letter to open it
+        Menu fileMenu = new Menu("_File");
+
+        //Menu items
+        MenuItem newMi= new MenuItem("New...");
+        newMi.setOnAction(e -> System.out.println("Create new Project"));
+
+        MenuItem openMi = new MenuItem("Open...");
+        openMi.setOnAction(e -> System.out.println("Open existing project"));
+
+        MenuItem saveMi = new MenuItem("Save...");
+        saveMi.setOnAction(e -> System.out.println("Save project"));
+
+        //horizontal line -> separator
+        fileMenu.getItems().add(new SeparatorMenuItem());
+
+        MenuItem settingsMi = new MenuItem("Settings...");
+        settingsMi.setOnAction(e -> System.out.println("Open Settings"));
+
+        //horizontal line -> separator
+        fileMenu.getItems().add(new SeparatorMenuItem());
+
+        MenuItem exitMi = new MenuItem("Exit...");
+        exitMi.setOnAction(e -> System.out.println("Close Project"));
+
+        fileMenu.getItems().addAll(newMi, openMi, saveMi, settingsMi, exitMi);
+
+
+
+        //          edit menu
+        Menu editMenu = new Menu("_Edit...");
+
+        MenuItem cutMi = new MenuItem("Cut...");
+        cutMi.setOnAction(e -> System.out.println("Cut"));
+
+        MenuItem copyMi = new MenuItem("Copy...");
+        copyMi.setOnAction(e -> System.out.println("Copy"));
+
+        MenuItem pasteMi = new MenuItem("Paste");
+        pasteMi.setOnAction(e -> System.out.println("Paste"));
+        //To disable option -  will be grayed out
+        pasteMi.setDisable(true);
+
+        editMenu.getItems().addAll(cutMi, copyMi, pasteMi);
+
+
+        //Main menu bar
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu, editMenu);
+
+
+        layout = new BorderPane();
+        layout.setTop(menuBar);
+
+        scene = new Scene(layout, 400, 300);
         window.setScene(scene);
         window.show();
-    }
-
-    //Delete button clicked
-    private void addButtonClicked() {
-        Product product = new Product();
-        product.setName(nameInput.getText());
-        product.setPrice(Double.parseDouble(priceInput.getText()));
-        product.setQuantity(Integer.parseInt(quantityInput.getText()));
-
-        table.getItems().add(product);
-
-        //to remove data from text fields
-        nameInput.clear();
-        priceInput.clear();
-        quantityInput.clear();
-
-    }
-
-    //Add button clicked
-    private void deleteButtonClicked() {
-        ObservableList<Product> selectedProducts, allProducts;
-        allProducts = table.getItems();
-        selectedProducts = table.getSelectionModel().getSelectedItems();
-
-        allProducts.removeAll(selectedProducts);
-    }
-
-    private ObservableList<Product> getProduct(){
-        ObservableList<Product> products = FXCollections.observableArrayList();
-        products.add(new Product("Laptop", 859.00, 20));
-        products.add(new Product("Bouncy Ball", 2.49, 198));
-        products.add(new Product("Toilet", 99.00, 74));
-        products.add(new Product("The Notebook DVD", 19.99, 12));
-        products.add(new Product("Corn", 1.49, 856));
-
-        return products;
     }
 }
 
